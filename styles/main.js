db6af7f754a29d166bcae38a9aa645aafdd37b23
Pumang Kirange <kirangespumang03@gmail.com>
2024-12-27 22:37:02 +0530

    Initial commit

---
+++ styles/main.js
@@ -0,0 +1,109 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1f1f1f',
+    },
+    result: {
+        flex: 2,
+        marginBottom: 20,
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+        marginRight: 45,
+        marginLeft: 40
+    },
+
+    btnRow1: {
+        flex: 1,
+        justifyContent: "space-around",
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom: -40,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    btnRow2: {
+        flex: 1,
+        justifyContent: "space-around",
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom: -40,
+        marginLeft: 15,
+        marginRight: 15
+        
+    },
+    btnRow3: {
+        flex: 1,
+        justifyContent: "space-around",
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom: -40,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    btnRow4: {
+        flex: 1,
+        justifyContent: "space-around",
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom: -40,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    btnRow5: {
+        flex: 1,
+        justifyContent: "space-around",
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom:0,
+        marginLeft: 15,
+        marginRight: 15
+    },
+
+    btnGray: {
+        backgroundColor: '#bbbbbb',
+        width: '20%',
+        height: '55%',
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    btnHardGray: {
+        backgroundColor: '#636363',
+        width: '20%',
+        height: '55%',
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    btnYellow: {
+        backgroundColor: '#ff9c08',
+        width: '20%',
+        height: '55%',
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    btnGrayZero: {
+        backgroundColor: '#636363',
+        width: '45%',
+        height: '55%',
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    textLight: {
+        color: '#FFFFFF',
+        fontSize: 28
+    },
+    textDark: {
+        color: '#000000',
+        fontSize: 28
+    }
+
+
+});
+  
+
+export default styles;
